62c4b05c3656554558d779489512add4bbaf68b7
0avasns <protopap@gmail.com>
2019-05-31 10:56:25 +0200

    Added processing modes: correct/wrong, classification, user string

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ import glob
 class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
-        self.version = "2.3.2.1"
+        self.version = "3.0.0.0"
         self.company_name = "Athanassios Protopapas"
-        self.copyright = "(c) 2018 -- November 3rd"
+        self.copyright = "(c) 2019 -- May 31st"
         self.name = "CheckVocal"
 
 CV = Target(
     description = "DMDX Vocal Response & RT Check Utility",
-    version = "2.3.2.1",
+    version = "3.0.0.0",
     script = "CheckVocal.pyw",
     icon_resources = [(1, "cv.ico")],
     dest_base = "CheckVocal")
 
 CF = Target(
     description = "Audio File RT Check Utility",
-    version = "2.3.2.1",
+    version = "3.0.0.0",
     script = "CheckVocal.pyw",
     icon_resources = [(1, "cf.ico")],
     dest_base = "CheckFiles")
@@ -29,19 +29,19 @@ CF = Target(
 AZT = Target(
     description = "DMDX output converter: .azk to .txt format",
     version = "1.2.3.0",
-    script = "azk2txt.pyw",  
+    script = "azk2txt.pyw",
     icon_resources = [(1, "a2t.ico")],
     dest_base = "azk2txt")
 
 setup(
-      options={ "py2exe": 
+      options={ "py2exe":
                {"packages": ["encodings"],
                 "compressed": 1,
                 "optimize": 2}
               },
       windows=[CF,CF,AZT,CV], # makes no sense but whatever is first does not get an icon on the exe, so I duplicated it ...
       data_files=[
-                  (r'tcl\snacklib', glob.glob(r'c:\python27\tcl\snacklib\*')),
+                  (r'tcl\snacklib', glob.glob(r'C:\python27\tcl\snack2.2\*')),
                   ("icons",["cv.ico","cf.ico","a2t.ico"]),
                   (".",["README-CheckVocal.txt"])
                  ],
